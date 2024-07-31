b2a72aa8d33ede4f13115df75661d8aeafab7f5a
Mohamed Ahmed <ma07041705@gmail.com>
2024-07-31 14:29:33 +0300

    seeding the data in the database

--- seed.py
+++ seed.py
@@ -1,9 +1,9 @@
-import cloudinary
-import cloudinary.uploader
-import cloudinary
-import cloudinary.uploader
 from model import db, Product, Shop, Category
 from app import app
+import random
+import cloudinary
+import cloudinary.uploader
+
 
 # Configure Cloudinary
 cloudinary.config(
@@ -12,121 +12,79 @@ cloudinary.config(
     api_secret='zRPXOHtgbFiTbRoq3q7NGG3hLeg'
 )
 
-def upload_image_to_cloudinary(image_path, folder):
-    result = cloudinary.uploader.upload(
-        image_path,
-        folder=folder
-    )
-    return result['url']
+def upload_image_to_cloudinary(image_path):
+    response = cloudinary.uploader.upload(image_path, folder="shopHorizon")
+    return response['secure_url']
 
-with app.app_context():
-    print("Deleting data.....")
-    Product.query.delete()
-    Shop.query.delete()
-    Category.query.delete()
-    db.session.commit()
+def seed_data():
+    with app.app_context():
+        print("Clearing existing data...")
+        Product.query.delete()
+        Shop.query.delete()
+        Category.query.delete()
+        db.session.commit()
+        print("Data cleared.")
 
-    print("Creating data .....")
+        print("Adding categories...")
+        electronics = Category(name="Electronics", category_image=upload_image_to_cloudinary("https://ecelectronics.com/wp-content/uploads/2020/04/Modern-Electronics-EC-.jpg"))
+        clothing = Category(name="Clothing", category_image=upload_image_to_cloudinary("https://t3.ftcdn.net/jpg/02/10/85/26/360_F_210852662_KWN4O1tjxIQt8axc2r82afdSwRSLVy7g.jpg"))
+        sports_and_outdoors = Category(name="Sports and Outdoors", category_image=upload_image_to_cloudinary("https://img.freepik.com/free-photo/young-healthy-man-athlete-doing-exercise-with-ropes-gym-single-male-model-practicing-hard-training-his-upper-body-concept-healthy-lifestyle-sport-fitness-bodybuilding-wellbeing_155003-27879.jpg"))
+        furniture = Category(name="Furniture", category_image=upload_image_to_cloudinary("https://st4.depositphotos.com/1023934/37752/i/450/depositphotos_377527168-stock-photo-interior-design-modern-living-room.jpg"))
+        others = Category(name="Others", category_image=upload_image_to_cloudinary("https://www.shutterstock.com/image-photo/male-female-accessories-on-white-260nw-1888761748.jpg"))
 
-    s1 = Shop(name="Amazon")
-    s2 = Shop(name="Alibaba")
+        db.session.add_all([electronics, clothing, sports_and_outdoors, furniture, others])
+        db.session.commit()
+        print("Categories added.")
 
-    # Upload images for categories
-    c1_image_url = upload_image_to_cloudinary('https://i.ebayimg.com/thumbs/images/g/A4AAAOSwiYNmOozq/s-l640.jpg', folder='categories')
-    c2_image_url = upload_image_to_cloudinary('https://www.gearpatrol.com/wp-content/uploads/sites/2/2023/07/best-furniture-refresh-lead-1659534331-jpg.webp', folder='categories')
+        print("Adding shops...")
+        shop1 = Shop(name="Amazon")
+        shop2 = Shop(name="Alibaba")
+        shop3 = Shop(name="Shopify")
 
-    c1 = Category(name="Electronics", category_image=c1_image_url)
-    c2 = Category(name="Furniture", category_image=c2_image_url)
+        db.session.add_all([shop1, shop2, shop3])
+        db.session.commit()
+        print("Shops added.")
 
-    # Upload images for products
-    e1_image_url = upload_image_to_cloudinary('https://m-cdn.phonearena.com/images/articles/406312-image/Samsung-Galaxy-A05s.jpg',folder='products')
-    e2_image_url = upload_image_to_cloudinary('https://m.media-amazon.com/images/S/aplus-media-library-service-media/3967678b-5f5b-4a23-8d96-bab21662328c.__CR0,0,970,600_PT0_SX970_V1___.jpg', folder='products')
-    f1_image_url = upload_image_to_cloudinary('https://cdn.fairdealfurniture.co.ke/wp-content/uploads/2024/05/10105626/1-4-jpg.webp',folder='products')
-    f2_image_url = upload_image_to_cloudinary('https://furniturepalacekenya.com/wp-content/uploads/2023/03/CAIN-BED-3-1-800x533.jpg',folder='products')
+        print("Adding products...")
+        
+       
+        product_templates = [
+            {"name": "Samsung A05", "category": electronics, "product_image": "https://m-cdn.phonearena.com/images/articles/406312-image/Samsung-Galaxy-A05s.jpg"},
+            {"name": "Hp EliteBook 840 G3", "category": electronics, "product_image": "https://m.media-amazon.com/images/S/aplus-media-library-service-media/3967678b-5f5b-4a23-8d96-bab21662328c.__CR0,0,970,600_PT0_SX970_V1___.jpg"},
+            {"name": "Cannon E0S R7", "category": electronics, "product_image": "https://t3.ftcdn.net/jpg/00/79/36/04/360_F_79360425_13tH0FGR7nYTNlXWKOWtLmzk7BAikO1b.jpg"},
+            {"name": "Quartz", "category": electronics, "product_image": "https://img.kwcdn.com/product/fancy/c8614497-5aec-4ec3-8562-ec12f5f61f91.jpg"},
+            {"name": "Men's T-shirt", "category": clothing, "product_image": "https://files.sophie.co.ke/2024/02/1880922072_179_7365-595x595.webp"},
+            {"name": "Women's Dress", "category": clothing, "product_image": "https://st.depositphotos.com/2515135/2791/v/950/depositphotos_27912009-stock-illustration-woman-beautiful-dresses-on-hanger.jpg"},
+             {"name": "Kids Cloths", "category": clothing, "product_image": "https://www.sheknows.com/wp-content/uploads/2023/09/Screen-Shot-2023-09-21-at-2.42.38-PM.png?w=1440"},
+            {"name": "Yoga Mat", "category": sports_and_outdoors, "product_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6Ebv5b152nJHbnWRdnadrzr-PkwZ3m8s9Tg&s"},
+            {"name": "Ball", "category": sports_and_outdoors, "product_image": "https://cdn.britannica.com/51/190751-131-B431C216/soccer-ball-goal.jpg"},
+            {"name": "Weight lfting", "category": sports_and_outdoors, "product_image": "https://5.imimg.com/data5/SELLER/Default/2024/3/405978903/CC/UB/QY/150323968/weight-lifting-equipment-500x500.jpg"},
+            {"name": "Office Chair", "category": furniture, "product_image": "https://dignity.co.ke/wp-content/uploads/2024/05/HIGH-BACK-OFFICE-CHAIR-BLACK-GREEN-DG-3004-1-657x544.webp"},
+            {"name": "HarMony King Bed", "category": furniture, "product_image": "https://furniturepalacekenya.com/wp-content/uploads/2023/05/CF-9148-P-DOREL.jpg-800x533.jpg"},
+            {"name": "5-Seater Corporate Sofa", "category": furniture, "product_image": "https://nicmaahomeandofficefurniturelimited.co.ke/wp-content/uploads/2023/10/Modern-Design-Commercial-Furniture-Office-Sofa-Set-Sofa-Chair-Office-Seating.webp"},
+            {"name": "table", "category": furniture, "product_image": "https://i.pinimg.com/736x/1e/9f/26/1e9f260095ba08b16b1d2783b22b4cba.jpg"},
+            {"name": "Jewelry", "category": others, "product_image": "https://nypost.com/wp-content/uploads/sites/2/2023/11/Untitled-design-42.jpg"},
+            {"name": "Phone Covers", "category": others, "product_image": "https://t4.ftcdn.net/jpg/03/68/09/49/360_F_368094934_RLbGLbrs68hPicpZd1vg7rn9njFgZRCr.jpg"},
+            {"name": "Phone Charger", "category": others, "product_image": "https://imageio.forbes.com/specials-images/imageserve/1048621702/USB-cable-charger-for-a-smartphone/960x0.jpg?format=jpg&width=960"},
+            {"name": "Speaker", "category": others, "product_image": "https://images.news18.com/ibnlive/uploads/2014/09/speakers-020914.jpg?impolicy=website&width=640&height=480"},
+        ]
 
-    e1 = Product(
-        name="Samsung A05",
-        price=30000.00,
-        ratings=4.7,
-        mode_of_payment="After delivery",
-        categoryId=1,
-        shopId=1,
-        product_image=e1_image_url
-    )
-    e2 = Product(
-        name="Samsung A05",
-        price=33000.00,
-        ratings=4.7,
-        mode_of_payment="After delivery",
-        categoryId=1,
-        shopId=2,
-        product_image=e1_image_url
-    )
-    e3 = Product(
-        name="Hp EliteBook 840 G3",
-        price=29999.00,
-        ratings=4,
-        mode_of_payment="After Delivery",
-        categoryId=1,
-        shopId=1,
-        product_image=e2_image_url
-    )
-    e4 = Product(
-        name="Hp EliteBook 840 G3",
-        price=29000.00,
-        ratings=4,
-        mode_of_payment="After Delivery",
-        categoryId=1,
-        shopId=2,
-        product_image=e2_image_url
-    )
-    f1 = Product(
-        name="5-Seater Corporate Sofa",
-        price=69999.00,
-        ratings=3,
-        mode_of_payment="Upfront",
-        categoryId=2,
-        shopId=1,
-        product_image=f1_image_url
-    )
-    f2 = Product(
-        name="5-Seater Corporate Sofa",
-        price=70000.00,
-        ratings=3,
-        mode_of_payment="Upfront",
-        categoryId=2,
-        shopId=2,
-        product_image=f1_image_url
-    )
-    f3 = Product(
-        name="HarMony King Bed Only",
-        price=75000.00,
-        ratings=5,
-        categoryId=2,
-        mode_of_payment="After Delivery",
-        shopId=1,
-        product_image=f2_image_url
-    )
-    f4 = Product(
-        name="HarMony King Bed Only",
-        price=75000.00,
-        ratings=5,
-        categoryId=2,
-        shopId=2,
-        mode_of_payment="Upfront",
-        product_image=f2_image_url
-    )
+        
+        for template in product_templates:
+            for shop in [shop1, shop2, shop3]:
+                product = Product(
+                    name=template["name"],
+                    price=round(random.uniform(10.0, 1000.0), 2),
+                    ratings=round(random.uniform(3.0, 5.0), 1),
+                    mode_of_payment=random.choice(["Upfront", "After Delevery"]),
+                    categoryId=template["category"].id,
+                    shopId=shop.id,
+                    product_image=upload_image_to_cloudinary(template["product_image"])
+                )
+                db.session.add(product)
 
-    categories = [c1, c2]
-    shops = [s1, s2]
-    electronics = [e1, e2, e3, e4]
-    furnitures = [f1, f2, f3, f4]
+        db.session.commit()
+        print("Products added.")
 
-    db.session.add_all(electronics)
-    db.session.add_all(furnitures)
-    db.session.add_all(shops)
-    db.session.add_all(categories)
-
-    db.session.commit()
-
-    print("Seeding done")
+if __name__ == "__main__":
+    seed_data()
