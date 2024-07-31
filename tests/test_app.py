aaa9df93b8a298d65225fdbf1c54a76490c76bb6
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-07-31 21:30:39 +0300

    add test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,7 @@
-from app import index,app
-from model import Searches
-import pytest
+from app import index
 
 
 
 def test_index():
     assert index() == "Hello, world!"
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        with app.app_context():
-            yield client
-
-def test_search_history_endpoint_exists(client):
-    response = client.get('/searchhistory')
-    assert response.status_code == 200
-    assert isinstance(response.get_json(), list)
