d714e14ded396fee09d17577326ca4e46f3f6729
Mohamed Ahmed <ma07041705@gmail.com>
2024-08-04 20:04:15 +0300

    testing files

--- tests/test_history.py
+++ tests/test_history.py
@@ -1,5 +1,5 @@
-from app import app
 from model import db, User, Searches
+from app import app
 from flask_jwt_extended import create_access_token
 import pytest
 import uuid  # Import the uuid module
@@ -13,14 +13,12 @@ def client():
             if existing_user:
                 db.session.delete(existing_user)
                 db.session.commit()
-            
+
             user = User(first_name="Test", last_name="User", email=unique_email, password="password")
             db.session.add(user)
             db.session.commit()
             token = create_access_token(identity=user.id)
-
             yield client, token
-
 def test_search_history_endpoint_exists(client):
     client, token = client
     response = client.get('/searchhistory', headers={'Authorization': f'Bearer {token}'})
