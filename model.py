96a8c2658666463184aae70004ea2ce8ebded5f6
Mohamed Ahmed <ma07041705@gmail.com>
2024-08-05 14:06:32 +0300

    fixing the error

--- model.py
+++ model.py
@@ -1,18 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
 from datetime import datetime
-import re
 
 metadata = MetaData(
     naming_convention={
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     }
 )
-
 db = SQLAlchemy(metadata=metadata)
-
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
@@ -26,15 +22,8 @@ class User(db.Model, SerializerMixin):
     reset_token_expiry = db.Column(db.DateTime, nullable=True)
     searches = db.relationship("Searches", back_populates="user", cascade="all, delete-orphan")
     serialize_rules = ("-searches.user",)
-
-    @validates('email')
-    def validate_email(self, key, email):
-        assert re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', email), "Invalid email format"
-        return email
-
     def __repr__(self):
         return f'<User {self.id}, {self.first_name}, {self.last_name}>'
-
 class Product(db.Model, SerializerMixin):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
@@ -48,34 +37,23 @@ class Product(db.Model, SerializerMixin):
     searches = db.relationship("Searches", back_populates="product")
     shop = db.relationship("Shop", back_populates="products")
     serialize_rules = ("-searches", "-shop.products",)
-
-    @validates('price')
-    def validate_price(self, key, price):
-        assert price >= 0, "Price must be non-negative"
-        return price
-
     def __repr__(self):
         return f'<Product {self.id}, {self.name}, {self.price}>'
-
 class Shop(db.Model, SerializerMixin):
     __tablename__ = "shops"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     products = db.relationship("Product", back_populates="shop")
     serialize_rules = ("-products.shop",)
-
     def __repr__(self):
         return f'<Shop {self.id}, {self.name}>'
-
 class Category(db.Model, SerializerMixin):
     __tablename__ = "categories"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     category_image = db.Column(db.String, nullable=True)
-
     def __repr__(self):
         return f'<Category {self.id}, {self.name}>'
-
 class Searches(db.Model, SerializerMixin):
     __tablename__ = "searches"
     id = db.Column(db.Integer, primary_key=True)
@@ -86,6 +64,5 @@ class Searches(db.Model, SerializerMixin):
     user = db.relationship("User", back_populates="searches")
 
     serialize_rules = ("-product", "-user",)
-
     def __repr__(self):
         return f'<Search {self.id}, User {self.userId}, Product {self.productId}>'
