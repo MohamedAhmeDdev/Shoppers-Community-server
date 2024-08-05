5d0b720b07325239599a4ca6440257b6d929502e
Mohamed Ahmed <ma07041705@gmail.com>
2024-08-05 14:11:31 +0300

    category test

--- tests/test_category.py
+++ tests/test_category.py
@@ -7,7 +7,8 @@ def client():
         with app.app_context():
             db.create_all()
             yield client
-            
+            db.session.remove()
+            db.drop_all()
 
 @pytest.fixture
 def setup_categories(client):
