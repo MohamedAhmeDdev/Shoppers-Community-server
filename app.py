74b5997fe00582d56087fa921259845354c2b29e
Mohamed Ahmed <ma07041705@gmail.com>
2024-08-08 17:09:16 +0300

    updates

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from config import (
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shophorizon.db'
 app.config['MAIL_SERVER'] = MAIL_SERVER
 app.config['MAIL_PORT'] = MAIL_PORT
 app.config['MAIL_USERNAME'] = MAIL_USERNAME
